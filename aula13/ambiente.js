9a79054c4f0a80a998719c56f1252821177dcf94
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-07-23 19:03:50 -0400

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+console.log('WHILE')
+let contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+console.log('\nDO WHILE')
+contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
+
+console.log('\nEstruturado')
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
