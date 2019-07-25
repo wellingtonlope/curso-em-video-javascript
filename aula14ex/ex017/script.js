e8f40e3cd9cd9eb30d8694ecd2875db45af8cc90
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-07-25 18:52:27 -0400

    ex17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
+function tabuada() {
+    let num = document.querySelector('#num')
+    let tab = document.querySelector('#tabuada')
 
+    if (num.value.length === 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for (let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${i} = ${n * i}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+        }
+    }
+}
